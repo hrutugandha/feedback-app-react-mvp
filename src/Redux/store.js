a0f255fffd258adabb026c5696a16a5513729287
NehaC123-ux <hrutunishi19@gmail.com>
2025-06-26 09:29:17 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,25 @@
-import { legacy_createStore } from "redux";
-import reducer from "./reducer";
+// import { legacy_createStore } from "redux";
+// import reducer from "./reducer";
 
-const store = legacy_createStore(reducer);
+// const store = legacy_createStore(reducer);
 
-export default store;
+// export default store;
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import userReducer from './slices/userSlice';
+import feedbackReducer from './slices/feedbackSlice';
+import { legacy_createStore } from 'redux';
+
+
+
+const store = legacy_createStore(reducer)
+
+export default configureStore({
+  reducer: {
+    auth: authReducer,
+    users: userReducer,
+    feedback: feedbackReducer,
+  },
+});
